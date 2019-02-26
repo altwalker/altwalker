f04a53a5f93fe0ef1b1ea84d41ef5b38ead81171
Robert Dezmerean <robert.dezmerean@altom.com>
2019-02-26 17:01:49 +0200

    Update read_json

--- altwalker/model.py
+++ altwalker/model.py
@@ -15,7 +15,7 @@ def _read_json(path):
     with open(path) as fp:
         try:
             data = json.load(fp)
-        except Exception as error:
+        except ValueError as error:
             raise ValidationException("Invalid json file: {}: {}.".format(path, str(error)))
 
     return data
