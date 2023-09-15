c4eb6a56ac2b5188606f2093a89f6657806e7574
Robert-96 <dezmereanrobert@gmail.com>
2023-09-15 21:54:28 +0100

    Update walker.py

--- altwalker/walker.py
+++ altwalker/walker.py
@@ -1,7 +1,10 @@
+import logging
 import traceback
 
 from altwalker.exceptions import GraphWalkerException
 from altwalker.reporter import Reporter
+
+logger = logging.getLogger(__name__)
 
 
 class Walker:
@@ -14,11 +17,8 @@ class Walker:
         self._planner = planner
         self._executor = executor
         self._reporter = reporter
-
         self._status = None
-
-        # a list of models to tearDown
-        self._models = []
+        self._models = []  # a list of models to tearDown
 
     def __iter__(self):
         self._reporter.start()
@@ -49,8 +49,24 @@ class Walker:
             if not step.get("name"):
                 continue
 
+            self._status = self._beforeStep()
+            if not self._status:
+                break
+
+            self._status = self._beforeStep(step["modelName"])
+            if not self._status:
+                break
+
             self._status = self._run_step(step)
             step["status"] = self._status
+
+            self._status = self._afterStep(step["modelName"])
+            if not self._status:
+                break
+
+            self._status = self._afterStep()
+            if not self._status:
+                break
 
             yield step
 
@@ -67,6 +83,64 @@ class Walker:
         """The status of the current run."""
 
         return self._status
+
+    def _update_data(self, data_before, data_after):
+        if not data_after:
+            return
+
+        for key, value in data_after.items():
+            if key not in data_before or data_before[key] != value:
+                self._planner.set_data(key, value)
+
+    def _execute_fixture(self, fixture_name, model_name=None, data=None, step=None):
+        step = {"type": "fixture", "name": fixture_name}
+        if model_name:
+            step["modelName"] = model_name
+
+        if not self._executor.has_step(step.get("modelName"), step.get("name")):
+            return True
+
+        return self._executor.execute_step(step.get("modelName"), step.get("name"), data=data, step=step)
+
+    def _execute_step(self, step):
+        data_before = self._planner.get_data()
+
+        self._reporter.step_start(step)
+        step_result = self._executor.execute_step(step.get("modelName"), step.get("name"), data=data_before, step=step)
+        self._reporter.step_end(step, step_result)
+
+        data_after = step_result.get("data")
+        self._update_data(data_before, data_after)
+
+        error = step_result.get("error")
+        if error:
+            self._planner.fail(error["message"])
+
+        return error is None
+
+    def _execute_test_step(self, step):
+        if not step.get("name"):
+            # A step without name is added into the model only for convenience
+            pass
+
+        self._status = self._before_step()
+        if not self._status:
+            return False
+
+        self._status = self._before_step(step["modelName"])
+        if not self._status:
+            return False
+
+        self._status = self._execute_step(step)
+        step["status"] = self._status
+
+        self._status = self._after_step(step["modelName"])
+        if not self._status:
+            return
+
+        self._status = self._after_step()
+        if not self._status:
+            return
 
     def _setUpRun(self):
         step = {
@@ -118,13 +192,29 @@ class Walker:
 
         return status
 
-    def _update_data(self, data_before, data_after):
-        if not data_after:
-            return
+    def _beforeStep(self, model=None, step=None):
+        step = {
+            "type": "fixture",
+            "name": "beforeStep",
+        }
+        if model:
+            step["modelName"] = model
 
-        for key, value in data_after.items():
-            if key not in data_before or data_before[key] != value:
-                self._planner.set_data(key, value)
+        status = self._run_step(step, optional=True)
+
+        return status
+
+    def _afterStep(self, model=None, step=None):
+        step = {
+            "type": "fixture",
+            "name": "afterStep",
+        }
+        if model:
+            step["modelName"] = model
+
+        status = self._run_step(step, optional=True)
+
+        return status
 
     def _execute_step(self, step):
         data_before = self._planner.get_data()
